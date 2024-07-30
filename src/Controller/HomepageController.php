2610e6e8a5cbcd8925ae7bd45f531e6bc0ef91a1
deoliveira-mallaury <mallaury.de.oliveira.pro@gmail.com>
2024-07-30 15:18:35 +0200

    feat: navbar repsonsive

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,24 +2,32 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use MobileDetectBundle\DeviceDetector\MobileDetectorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function index(): Response
+    public function index(MobileDetectorInterface $mobileDetector): Response
     {
+        $isMobile = $mobileDetector->isMobile();
+        $isTablet = $mobileDetector->isTablet();
+        $isIphone = $mobileDetector->is('iPhone');
+        var_dump($isMobile);
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
+            'isMobile' => $isMobile,
+            'isTablet' => $isTablet,
+            'isIphone' => $isIphone,
         ]);
     }
     #[Route('/services', name: 'app_services')]
     public function services(): Response
     {
         return $this->render('services/index.html.twig', [
-            'controller_name' => 'ServicesController',
+            'controller_name' => 'ServicesController'
         ]);
     }
 }
